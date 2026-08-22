04da552cb270801aa3d1ed45e2e7696471c167dc
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-22 17:09:06 +0530

    Add production URL to CORS middleware

--- backend122/backend/index.js
+++ backend122/backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5175"],
+  origin: ["http://localhost:5173", "http://localhost:5175","https://globetrotter-nyxo.onrender.com"],
   credentials: true,
 }));
 app.use(express.json());
